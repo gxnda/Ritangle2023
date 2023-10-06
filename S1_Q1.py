3d3a31f7f729c1680cc15b92041abbd457a6ab61
gxnda <116901054+gxnda@users.noreply.github.com>
2023-10-06 18:26:14 +0000

    Done Q1, haven't checked yet

---
+++ S1_Q1.py
@@ -0,0 +1,28 @@
+from itertools import permutations
+from functools import reduce as fold
+
+
+def get_all_permutations(string: str) -> list:
+    # itertools coming in clutch
+    all_permutations = [
+        "".join(i) for i in list(permutations(string, len(string)))
+    ]
+    return all_permutations
+
+
+def product_of_string(string: str) -> int:
+    def _times(num1, num2):
+        return num1 * num2
+    digits = [int(string[i]) for i in range(len(string))]
+    return fold(_times, digits)
+
+
+perms = get_all_permutations("12345678")
+
+counter = 0
+for string in perms:
+    left = string[:5]
+    right = string[5:]
+    if product_of_string(left) < product_of_string(right):
+        counter += 1
+print(counter / len(perms))
